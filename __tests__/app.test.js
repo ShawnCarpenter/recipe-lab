d480fe2ef8399cd1ab4ecd0d2976e28fd7f773e7
Shawn Carpenter <shawn.carpeenter@gmail.com>
2020-09-22 14:37:53 -0700

    added logs model and two tests'

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const Recipe = require('../lib/models/recipe');
+const Log = require('../lib/models/log');
 
 describe('recipe-lab routes', () => {
   beforeEach(() => {
@@ -101,4 +102,60 @@ describe('recipe-lab routes', () => {
       .delete(`/api/v1/recipes/${recipe.id}`)
       .then(res => expect(res.body).toEqual(recipe));
   });
+
+  it('creates a log', async() => {
+
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+    return request(app)
+      .post('/api/v1/logs')
+      .send({
+        recipeId: recipe.id,
+        dateOfEvent: '2020-09-22',
+        notes: 'Yummy, would make again',
+        rating: 10
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          recipeId: recipe.id,
+          dateOfEvent: '2020-09-22T07:00:00.000Z',
+          notes: 'Yummy, would make again',
+          rating: 10
+        });
+      });
+  });
+
+  it('gets all logs', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+    const logs = await Promise.all([
+      { recipeId: recipe.id, dateOfEvent: '2020-09-22', notes: 'Yum', rating: 10 },
+      { recipeId: recipe.id, dateOfEvent: '2020-09-22', notes: 'Yuck', rating: 1 },
+      { recipeId: recipe.id, dateOfEvent: '2020-09-22', notes: 'OK', rating: 5 }
+    ].map(log => Log.insert(log)));
+
+    return request(app)
+      .get('/api/v1/logs')
+      .then(res => {
+        logs.forEach(log => {
+          expect(res.body).toContainEqual({ ...log, dateOfEvent: log.dateOfEvent.toISOString() });
+        });
+      });
+  });
+
 });
